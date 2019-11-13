f27974533394f8b025d43268a795b1cda3928cb3
ELREYS <gdilisa@hotmail.com>
2019-11-13 16:34:30 +0100

    added axios api setup and configuration

---
+++ src/api/yelp.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL:'https://api.yelp.com/v3/businesses',
+    headers:{
+        Authorization:'Bearer qU6Mrp9UMD9nMMpiv9Fezjr-W8OHdZUjbqEWCFEMOSUfhYB8qG8Z_vbLdoSuFC9KMA0WSNMTTYuE3CZK1WMXKAbhHBhoRX4CjPMtfrIDP9LsQo_2fQzvO2i4pubLXXYx'
+    }
+});
+
+
+
+
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,32 @@
 import React, {useState}from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
 import SearchBar from "../component/SearchBar";
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   [term,setTerm] = useState("");
-    return (
+  [results,setResults] = useState([]);
+    
+    searchApi= async () => {
+        const response  = await yelp.get('/search',{
+            params:{
+                limit: 50,
+                term,
+                location: "Basel",
+            }
+        });
+        setResults(response.data.businesses);
+    }
+
+  return (
     <View>
       <SearchBar 
       term={term} 
-      onTermChange={(newTerm) => setTerm(newTerm)}
-      onTermSubmit={() => console.log("Submited")
-      }
+      onTermChange={setTerm}
+      onTermSubmit={searchApi}
       ></SearchBar>
       <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      <Text>We have found {results.length}</Text>
     </View>
   );
 };
