377d28988591663142c9bc61335f8d189c45d5a4
ELREYS <gdilisa@hotmail.com>
2019-11-13 14:26:28 +0100

    setup react-navigation amd react-navigation-stack

---
+++ screens/SearchScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
+
+
+const SearchScreen = () =>{
+
+
+return (
+
+<Text>Search Screen</Text>
+
+);
+}
+
+
+const styles = StyleSheet.create({
+
+
+})
+
+
+export default SearchScreen;
